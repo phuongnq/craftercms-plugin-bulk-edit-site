5c0a11c98abd9db42a4c2e036620b7e49684ea13
Phil Nguyen <nquangphuong@gmail.com>
2021-10-07 16:22:55 +0700

    Update

--- sources/bulk-edit/src/BulkEditApp.js
+++ sources/bulk-edit/src/BulkEditApp.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button, Stack } from '@mui/material';
+import AutoAwesomeMotionOutlinedIcon from '@mui/icons-material/AutoAwesomeMotionOutlined';
 
 function BulkEditApp() {
   return (
     <Stack spacing={2} direction="row">
+      <AutoAwesomeMotionOutlinedIcon />
       <Button variant="contained">Bulk Edit</Button>
     </Stack>
   );
