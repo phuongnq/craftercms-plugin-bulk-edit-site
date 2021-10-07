ef720ac53dc805f8570070b5a50b17bfb551eb13
Phil Nguyen <nquangphuong@gmail.com>
2021-10-07 16:45:45 +0700

    Updates to index.js @ 2021.7.4 16:45

--- config/studio/plugins/js/org/craftercms/plugin/sidebar/bulkedit/index.js
+++ config/studio/plugins/js/org/craftercms/plugin/sidebar/bulkedit/index.js
@@ -1,77 +1,7 @@
 var React = craftercms.libs.React && Object.prototype.hasOwnProperty.call(craftercms.libs.React, 'default') ? craftercms.libs.React['default'] : craftercms.libs.React;
 var { Stack, Button } = craftercms.libs.MaterialUI;
-import _jsxRuntime from 'react/jsx-runtime';
-import _utils from '@mui/material/utils';
-
-function getDefaultExportFromCjs (x) {
-	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
-}
-
-function createCommonjsModule(fn, basedir, module) {
-	return module = {
-		path: basedir,
-		exports: {},
-		require: function (path, base) {
-			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-		}
-	}, fn(module, module.exports), module.exports;
-}
-
-function commonjsRequire () {
-	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
-}
-
-var interopRequireDefault = createCommonjsModule(function (module) {
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {
-    "default": obj
-  };
-}
-
-module.exports = _interopRequireDefault;
-module.exports["default"] = module.exports, module.exports.__esModule = true;
-});
-
-var createSvgIcon = createCommonjsModule(function (module, exports) {
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "default", {
-  enumerable: true,
-  get: function () {
-    return _utils.createSvgIcon;
-  }
-});
-});
-
-var require$$0 = createSvgIcon;
-
-var AutoAwesomeMotionOutlined = createCommonjsModule(function (module, exports) {
-
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _createSvgIcon = interopRequireDefault(require$$0);
-
-
-
-var _default = (0, _createSvgIcon.default)( /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-  d: "M14 2H4c-1.1 0-2 .9-2 2v10h2V4h10V2zm4 4H8c-1.1 0-2 .9-2 2v10h2V8h10V6zm2 4h-8c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2zm0 10h-8v-8h8v8z"
-}), 'AutoAwesomeMotionOutlined');
-console.log(_default);
-exports.default = _default;
-});
-
-var AutoAwesomeMotionOutlinedIcon = /*@__PURE__*/getDefaultExportFromCjs(AutoAwesomeMotionOutlined);
 
 function BulkEditApp() {
-  console.log(AutoAwesomeMotionOutlinedIcon);
-  console.log("BulkEditApp");
   return /*#__PURE__*/React.createElement(Stack, {
     spacing: 2,
     direction: "row"
