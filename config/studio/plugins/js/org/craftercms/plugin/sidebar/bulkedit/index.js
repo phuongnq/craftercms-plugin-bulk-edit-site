e3ef15f856f2d412a9e27f734ce3c56850f4f253
Phil Nguyen <nquangphuong@gmail.com>
2021-10-19 15:36:30 +0700

    Updates to index.js @ 2021.19.2 15:36

--- config/studio/plugins/js/org/craftercms/plugin/sidebar/bulkedit/index.js
+++ config/studio/plugins/js/org/craftercms/plugin/sidebar/bulkedit/index.js
@@ -22447,13 +22447,14 @@ function DataSheet(_ref) {
     };
   }, []);
   e__default.useEffect(function () {
+    var subscriber;
+
     _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
-      var sub;
       return regeneratorRuntime.wrap(function _callee2$(_context2) {
         while (1) {
           switch (_context2.prev = _context2.next) {
             case 0:
-              sub = contentTypeSub.subscribe( /*#__PURE__*/function () {
+              subscriber = contentTypeSub.subscribe( /*#__PURE__*/function () {
                 var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(value) {
                   var config, headerList, items, paths, dtRows, i, path, content, row;
                   return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -22513,15 +22514,18 @@ function DataSheet(_ref) {
                   return _ref3.apply(this, arguments);
                 };
               }());
-              return _context2.abrupt("return", sub.unsubscribe());
 
-            case 2:
+            case 1:
             case "end":
               return _context2.stop();
           }
         }
       }, _callee2);
     }))();
+
+    return function () {
+      subscriber.unsubscribe();
+    };
   }, []);
 
   var handleEditRowsModelChange = function handleEditRowsModelChange(model) {
